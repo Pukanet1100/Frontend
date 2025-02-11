0d270015597b9ad0f4ad41bc487428a2095c62c9
BOSS <sixergouache@gmail.com>
2025-02-11 21:43:11 +0700

    fix search when search model not filter model, delay to GET data 0.8 sec

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -11,21 +11,30 @@ export class SearchComponent {
   selectedHandset: string = '';
   handsets: any[] = [];
   showDropdown: boolean = false;
+  searchTimeout: any;
 
   constructor(private apiService: ApiService) {}
 
   onSearch(): void {
-    if (this.selectedHandset.trim()) {
-      this.apiService.searchHandset(this.selectedHandset).subscribe((data) => {
-        this.handsets = data;
-        this.showDropdown = this.handsets.length > 0;
-      });
-    } else {
-      this.handsets = [];
-      this.showDropdown = false;
-    }
+    clearTimeout(this.searchTimeout);
+  
+    this.searchTimeout = setTimeout(() => {
+      if (this.selectedHandset.trim()) {
+        this.apiService.searchHandset(this.selectedHandset).subscribe((data) => {
+          const query = this.selectedHandset.trim().toLowerCase();
+          this.handsets = data.filter((handset: any) =>
+            handset.commercialName.toLowerCase().includes(query)
+          );
+  
+          this.showDropdown = this.handsets.length > 0;
+        });
+      } else {
+        this.handsets = [];
+        this.showDropdown = false;
+      }
+    }, 800);
   }
-
+  
   selectHandset(handset: any): void {
     this.selectedHandset = handset.commercialName;
     this.showDropdown = false;
